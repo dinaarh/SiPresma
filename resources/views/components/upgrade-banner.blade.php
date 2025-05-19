c6ba3f00074517e3a708e94e4af1288983eaab53
Kevin <kevinadikasaputragithub@gmail.com>
2025-05-19 20:05:48 +0700

    :sparkles: Add: another components for dashboard

---
+++ resources/views/components/upgrade-banner.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-purple-600 text-white p-4 rounded-lg shadow-lg">
+    <h4 class="font-semibold">Upgrade to Pro</h4>
+    <p class="text-sm">Get 1 month free on annual subscription</p>
+    <button class="bg-white text-purple-600 mt-2 p-2 rounded-lg">Upgrade Today!</button>
+</div>
