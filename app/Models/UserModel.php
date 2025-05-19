2b9375488b1dd9d4ccd1216f0240105a410703cd
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-05-19 21:21:36 +0700

    :recycle: Refactor: rename User class to UserModel and add relationship methods

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class UserModel extends Model
 {
     use HasFactory;
     protected $table = 'm_users';
@@ -19,4 +19,42 @@ class User extends Model
         'role',
         'status_akun'
     ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    public function admin()
+    {
+        return $this->hasOne(AdminModel::class, 'user_id', 'user_id');
+    }
+
+    public function dosenPembimbing()
+    {
+        return $this->hasOne(DosenPembimbingModel::class, 'user_id', 'user_id');
+    }
+
+    public function mahasiswa()
+    {
+        return $this->hasOne(MahasiswaModel::class, 'user_id', 'user_id');
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin()->exists();
+    }
+
+    public function isDosenPembimbing()
+    {
+        return $this->dosenPembimbing()->exists();
+    }
+
+    public function isMahasiswa()
+    {
+        return $this->mahasiswa()->exists();
+    }
 }
