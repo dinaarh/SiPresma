1cc90aa671b8875a6787b077dcf609e2893b2f8e
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-05-27 07:07:40 +0700

    refactor: update sidebar menu-item component to use route prop and improve structure

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
+    <section>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
             <x-dashboard.count-card title="Total Mahasiswa" count="1,000" icon="fa-solid fa-users" />
             <x-dashboard.count-card title="Total Kelompok" count="200" icon="fa-solid fa-medal" />
             <x-dashboard.count-card title="Total Lomba" count="50" icon="fa-solid fa-trophy" />
             <x-dashboard.count-card title="Total Prestasi" count="200" icon="fa-solid fa-medal" />
         </div>
-    </div>
+    </section>
 @endsection
--- resources/views/components/sidebar/menu-item.blade.php
+++ resources/views/components/sidebar/menu-item.blade.php
@@ -1,5 +1,6 @@
 @props([
-    'href' => '/',
+    'route' => 'dashboard',
+    'currentRoute' => null,
     'icon' => 'fa-solid fa-circle-info',
     'title' => 'Default Title',
     'color' => 'gray-500',
@@ -8,16 +9,18 @@
 ])
 
 @php
-    $isActive = request()->url() === $href;
-    $baseClasses = 'flex items-center p-2 transition-colors duration-200 ease-in-out rounded-r-lg group';
-    $linkClasses = $isActive ? "{$baseClasses} {$activeClass}" : "{$baseClasses} {$inactiveClass} hover:bg-gray-100";
-    $textClasses = $isActive ? 'text-primary' : "text-{$color}";
+    $route = empty($route) ? 'dashboard' : $route;
+    $isActive = request()->routeIs($route);
 @endphp
 
-<li>
-    <a href="{{ $href }}" class="{{ $linkClasses }}"
-        @if ($isActive) aria-current="page" @endif>
-        <i class="mx-2 {{ $textClasses }} {{ $icon }}" aria-hidden="true"></i>
-        <span class="ms-1 text-sm font-medium {{ $textClasses }}">{{ $title }}</span>
-    </a>
-</li>
+
+<a href="{{ route($route) }}" @class([
+    'flex items-center p-2 transition-colors duration-200 ease-in-out rounded-r-lg group',
+    $activeClass => $isActive,
+    "{$inactiveClass} hover:bg-gray-100" => !$isActive,
+]) @if ($isActive) aria-current="page" @endif>
+    <i class="mx-2 {{ $icon }} @if ($isActive) text-primary @else text-{{ $color }} @endif"
+        aria-hidden="true"></i>
+    <span
+        class="ms-1 text-sm font-medium @if ($isActive) text-primary @else text-{{ $color }} @endif">{{ $title }}</span>
+</a>
