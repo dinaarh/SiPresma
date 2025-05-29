a9e07340fb496956ccc9fd4b643955d3f101d247
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-05-30 01:22:22 +0700

    feat: add Kelompok, Kompetensi, and Minat controllers with resource methods

--- app/Http/Controllers/KompetensiController.php
+++ app/Http/Controllers/KompetensiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProdiController extends Controller
+class KompetensiController extends Controller
 {
     /**
      * Display a listing of the resource.
